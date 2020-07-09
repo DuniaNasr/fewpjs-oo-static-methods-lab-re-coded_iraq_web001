967470e6fa29b4055c72a5d6ce886afc365f6e9a
Dunia Nasr <dunia.nasr777@gmail.com>
2020-07-09 18:45:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 class Formatter {
   //add static methods here
+  static capitalize(a){
+     if (typeof a !== 'string') return ''
+    return a.charAt(0).toUpperCase() + a.slice(1)
+  }
+   static sanitize(b){
+    
+     const chars = []
+    b.split('').forEach((char) => {
+      if (!['@', '#', '!', '$', '%', '^', '{', '}', '*', '(', ')'].includes(char) || ["'"].includes(char)) {
+        chars.push(char)
+      }
+    })
+   static titleize(){
+    
+  }
 }
